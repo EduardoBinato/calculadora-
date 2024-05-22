6df35d0b8cfb5f43387df4a4cadb2d87a8a09acf
Binato <Binato@DESKTOP-QQO559V>
2024-05-22 20:42:14 -0300

    Adicionar arquivos de projeto.

---
+++ calculadora 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Coloque um numero: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Coloque outro numero: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num1 + num2);
+
+            Console.ReadLine();
+
+        }
+    }
+}
